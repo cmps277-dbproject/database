a052ff7a5c5b15c6830aa6131f0ed9ac5cda6d9f
smb46 <73215324+smb46@users.noreply.github.com>
2021-05-09 16:52:44 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -153,7 +153,7 @@ CREATE TABLE clothesshop.Manages
     FOREIGN KEY(ManagerWorkerID) REFERENCES Manager(WorkerID)
 );
 CREATE VIEW `SuppliersClothes` AS 
-SELECT Name, Phone, Country, State, ZIP, clothesSupply.ClothesID, Brand, Category, Color, Gender, Material, Price, suppliers.SupplierID
+SELECT Name, Phone, Country, State, ZIP, clothesSupply.ClothesID, Brand, Category, Color, Gender, Material, Price
 FROM ( SELECT SupplierID, clothes.ClothesID, Brand, Category, ClientID, Color, Gender, Material, Price, WorkerID 
 	FROM clothes 
 	INNER JOIN supply ON clothes.ClothesID=supply.ClothesID ) AS clothesSupply 
