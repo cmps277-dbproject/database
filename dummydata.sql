7e200266fd9af3b7e00c06d483a30dbfae48b8c1
elmarwanos <marwanbassam13@gmail.com>
2021-05-10 04:48:41 +0300

    Completed dummy data

--- dummydata.sql
+++ dummydata.sql
@@ -1,3 +1,4 @@
+------------------------------WORKERS----------------------------------
 INSERT INTO clothesshop.Employees
 (`WorkerID`, `Salary`, `Type`, `Schedule`, `Department`) 
 VALUES 
@@ -8,13 +9,13 @@ VALUES
 ('5', '5000', 'Assistant Manager', 'Weekdays', 'Management')
 
 INSERT INTO clothesshop.Worker
-(`WorkerID`, `Name`, `Address`, `Phone`, `Email`) 
+(`Name`, `Address`, `Phone`, `Email`) 
 VALUES 
-('1', 'Keanu Reeves', 'America', '239 709-6359', 'keanu@reeves.com'), 
-('2', 'Jack Black', 'England', '884 604-0425', 'jack@black.com'),
-('3', 'Joaquin Phoenix', 'America', '205 590-7140', 'joaquin@phoenix.org'), 
-('4', 'Jennifer Lawrence', 'America' ,'223 548-5220', 'jennifer@lawrence.com'), 
-('5', 'Lady Gaga', 'Atlantis', '214 156-4651', 'lady@gaga.com');
+( 'Keanu Reeves', 'America', '239 709-6359', 'keanu@reeves.com'), 
+( 'Jack Black', 'England', '884 604-0425', 'jack@black.com'),
+( 'Joaquin Phoenix', 'America', '205 590-7140', 'joaquin@phoenix.org'), 
+( 'Jennifer Lawrence', 'America' ,'223 548-5220', 'jennifer@lawrence.com'), 
+( 'Lady Gaga', 'Atlantis', '214 156-4651', 'lady@gaga.com');
 
 INSERT INTO clothesshop.Owner
 (`WorkerID`) 
@@ -32,20 +33,21 @@ VALUES
 ('1', '3'), 
 ('4', '3');
 
+-----------------------------CLOTHES-------------------------------------
 INSERT INTO clothesshop.Clothes
 ( `ClientID`, `WorkerID`, `Color`, `Brand`, `Material`, `Price`, `Gender`, `Category`) 
 VALUES 
 ( '1', '1', 'Yellow', 'H&M', 'silk', '22.22', 'F', 'dresses'),
-( '3', '1', 'Orange', 'gucci', 'cloth', '99.99', 'F', 'tops'), 
+( '1', '4', 'Orange', 'gucci', 'cloth', '99.99', 'F', 'tops'), 
 ( '3', '4', 'black', 'converse', 'jeans', '50.76', 'X', 'shoes'),
-( '3', '1', 'blue', 'nike', 'cotton', '12.00', 'X', 'hats'),
+(NULL,NULL, 'blue', 'nike', 'cotton', '12.00', 'X', 'hats'),
 ( '2', '4', 'blak', 'docker', 'leather', '49.99', 'F', 'pants'),
-( '2', '5', 'pink', 'polo', 'silk', '33.33', 'M', 'tops'), 
-( '3', '1', 'white', 'filas', 'leather', '66.66', 'M', 'shoes'),
-( '4', '2', 'black', 'hanes', 'cotton', '10.00', 'X', 'tops'),
-('5', '3', 'purple', 'H&M', 'silk', '59.00', 'F', 'dresses'),
-('5', '4', 'green', 'under armour', 'khaki', '22.34', 'F', 'pants'), 
-('3', '2', 'yellow', 'adidas', 'cotton', '44.44', 'X', 'hats');
+( '2', '4', 'pink', 'polo', 'silk', '33.33', 'M', 'tops'), 
+(NULL,NULL, 'white', 'filas', 'leather', '66.66', 'M', 'shoes'),
+(NULL,NULL, 'black', 'hanes', 'cotton', '10.00', 'X', 'tops'),
+( '5', '1', 'purple', 'H&M', 'silk', '59.00', 'F', 'dresses'),
+(NULL,NULL, 'green', 'under armour', 'khaki', '22.34', 'F', 'pants'), 
+( '3', '4', 'yellow', 'adidas', 'cotton', '44.44', 'X', 'hats');
 
 INSERT INTO clothesshop.Dresses 
 (`ClothesID`, `Length`, `Type`) 
@@ -77,3 +79,54 @@ VALUES
 ('5', '36in', 'slim', '30in', 'dress pants', '25in'),
 ('10', '29in', 'baggy', '30in', 'cargo', '27in');
 
+----------------------------CLIENTS/RECEIPTS--------------------------------
+INSERT INTO clothesshop.Receipt
+(`ClientID`, `Date_Time`, `MethodOfPayment`, `WorkerID`)  
+VALUES 
+('1', '1000-01-01 00:00:00.000000', 'Credit Card', '1'), 
+('2', '1000-01-01 00:00:00.000000', 'Credit Card', '4'), 
+('3', '1000-01-01 00:00:00.000000', 'Credit Card', '4'), 
+('1', '1000-01-01 00:00:00.000000', 'Cash', '4'), 
+('5', '1000-01-01 00:00:00.000000', 'Cash', '1');
+
+INSERT INTO clothesshop.ClothesReceipt
+(ClothesID, ReceiptID)
+VALUES
+('1','1'),
+('5','2'),
+('6','2'), 	-- might need to remove this
+('3','3'),
+('11','3'), -- might need to remove this
+('2','4'),
+('9','5');
+
+INSERT INTO clothesshop.Clients
+(`Name`, `Phone`, `Address`, `Email`) 
+VALUES 
+( 'Ali', '03000000', 'Lebanon', 'ali@gmail.com'), 
+( 'Bassam', '03000001', 'France', 'bassam@gmail.com'), 
+( 'Carla', '03000002', 'Saida', 'carla@gmail.com'), 
+( 'Daoud', '03000003', 'Sudan', 'daoud@gmail.com'), 
+( 'Ema', '03000004', 'Brazil', 'ema@gmail.com');
+
+-------------------------------SUPPLIERS---------------------------------
+INSERT INTO clothesshop.Suppliers
+(`Name`, `Phone`, `Country`, `State`, `ZIP`)
+VALUES
+('AliBaba', '0014 3185102', 'China', 'Xiang', '01120'),
+('Dior', '0014 4484512', 'Portugal', 'Lisbon', '01120');
+
+INSERT INTO clothesshop.Supply
+(`ClothesID`, `ReceiptID`)
+VALUES
+('1', '1'),
+('2', '1'),
+('3', '2'),
+('4', '1'),
+('5', '2'),
+('6', '2'),
+('7', '1'),
+('8', '2'),
+('9', '2'),
+('10', '1'),
+('11', '1');
